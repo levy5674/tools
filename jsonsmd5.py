35af77788169d78e98230ac29c06a3befe854ce8
Josh Levy <jlevy@glgroup.com>
2015-02-24 13:57:58 -0600

    refactor jsonsmd5

--- jsonsmd5.py
+++ jsonsmd5.py
@@ -7,6 +7,7 @@ import argparse
 from glob import glob
 import json
 from md5 import md5
+from sys import stdout
 
 def check_fp(f):
 	docs = map(json.loads, f)
@@ -17,10 +18,14 @@ def check_fn(filename):
 	with open(filename) as f:
 		return check_fp(f)
 
+def check_filenames(pattern, out=stdout):
+	files = sorted(glob(pattern))
+	for fn in files:
+		out.write("%s\t%s\n" % (fn, check_fn(fn)))
+
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser(description='Compute md5 of normalized jsons file')
 	parser.add_argument("-p", "--pattern", type=str, help="glob pattern of jsons files", default="*jsons")
 	args = parser.parse_args()
-	files = sorted(glob(args.pattern))
-	for fn in files:
-		print("%s\t%s" % (fn, check_fn(fn)))
+	check_filenames(args.pattern)
+	
